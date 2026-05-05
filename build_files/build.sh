5c00f0ecc9c4cf23dcfc56220d076e39729b979e
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-05-05 18:42:43 +1000

    Updating readme

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs/removes packages from fedora repos or flatpak
+# this installs/removes packages from fedora repositories or flatpak
 # example: dnf5 install -y tmux 
 
 # this removes package repositories
