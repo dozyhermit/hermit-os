0243bd62a6775b9e9ec40925ee90da2df8fe306f
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-08-11 21:31:54 +1000

    Missing -y commands on a few other thihngs

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,11 +20,8 @@ rm -rf /etc/yum.repos.d/google-chrome.repo
 dnf5 install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 dnf5 install -y "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
-dnf5 copr enable lizardbyte/beta
-
-dnf5 update -y
-dnf5 upgrade -y
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1 -y
+dnf5 copr enable lizardbyte/beta -y
 
 # this removes dnf packages
 dnf5 remove -y firefox firefox-langpacks malcontent-control yelp gnome-color-manager gnome-system-monitor gnome-tour gnome-disk-utility gnome-remote-desktop
